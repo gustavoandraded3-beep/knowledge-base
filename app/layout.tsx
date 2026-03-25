9a058b0e18989f24083969279f4dc7d80c739a0e
gustavoandraded3-beep <gustavo.andraded3@gmail.com>
2026-03-25 14:36:20 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./global.css";
 import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
